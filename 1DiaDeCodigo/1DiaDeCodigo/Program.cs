26af7edcd0e65151792c37c6abe007e607c446db
Willian Gonçalves <santoswil81@gmail.com>
2025-02-25 16:18:22 -0300

    First commit, first day of code

---
+++ 1DiaDeCodigo/1DiaDeCodigo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+// LINQ: LINQ (Language Integrated Query) é uma parte da plataforma .NET da Microsoft. 
+// Permite realizar consultas em várias fontes de dados, como bancos de dados relacionais,
+// coleções de objetos, documentos XML, entre outros. 
+
+// Expressões Lambda: São funções anônimas que podem conter expressões e declarações.
+// São úteis quando se precisa passar uma função como argumento para outro método.
+
+var fruits = new List<string>();
+fruits.Add("cereja");
+fruits.Add("abacaxi");
+fruits.Add("maçã");
+fruits.Add("pêssego");
+fruits.Add("mirtilo");
+fruits.Add("coco");
+fruits.Add("banana");
+
+// 1. Primeiro Elemento:
+Console.WriteLine(fruits.First());
+Console.WriteLine(fruits.FirstOrDefault("Não encontrei."));
+
+// 2. Verificar se um elemento existe:
+Console.WriteLine(fruits.Any(x => x == "cereja"));
+
+// 3. Verificar se todos os elementos são de um tipo:
+Console.WriteLine(fruits.All(x => x == "cereja"));
+
+// 4. Contar:
+Console.WriteLine(fruits.Count);
+Console.WriteLine(fruits.Count(x => x == "cereja"));
+
+// 5. ElementAt - índice:
+Console.WriteLine(fruits.ElementAt(5));
+
+// 6. Take:
+foreach (var fruit in fruits.Take(3)) // Pegando os 3 primeiros elementos.
+    Console.WriteLine(fruit);
+
+// 7. Where:
+foreach (var fruit in fruits
+        .Where(x => x == "cereja" || x == "banana"))
+    Console.WriteLine(fruit);
+
+// 8. Single:
+Console.WriteLine(fruits.Single(x => x == "coco"));
+Console.WriteLine(fruits.SingleOrDefault(x => x == "cereja"));
+
+// 9. Último elemento:
+Console.WriteLine(fruits.Last());
+
+// 10. Skip:
+foreach (var fruit in fruits.Skip(3))
+    Console.WriteLine(fruit);
---
+++ README.md
@@ -0,0 +1,4 @@
+# **#100DiasDeCodigo 💻📖**
+
+*100 dias de código. 100 dias aprimorando minhas habilidades. 100 dias obtendo muito mais conhecimento. 100 dias alinhado com o compromisso de mergulhar mais fundo no desenvolvimento em C#/.NET.* 
+
