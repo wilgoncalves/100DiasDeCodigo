5e148da74b2a94f5b2d7b0f87e130c9271bf0346
Willian Gonçalves <santoswil81@gmail.com>
2025-02-26 16:22:41 -0300

    Segundo dia de desafio - exercicios com linq

---
+++ 2DiasDeCodigo/2DiasDeCodigo/Program.cs
@@ -0,0 +1,47 @@
+﻿using _2DiasDeCodigo;
+
+// Consultas LINQ
+
+var songs = new List<Songs>
+{
+    new Songs(1, "Bohemian Rhapsody", "Queen", new DateTime(1975, 10, 31)),
+    new Songs(2, "Newborn Me", "Angra", new DateTime(2014, 12, 17)),
+    new Songs(3, "November Rain", "Guns N' Roses", new DateTime(1991, 02, 18)),
+    new Songs(4, "The Show Must Go On", "Queen", new DateTime(1991, 10, 14))
+};
+
+Console.WriteLine("Songs released in 1991");
+var songs1991 = songs.Where(x => x.ReleaseDate.Year == 1991).ToList();
+
+foreach (var items in songs1991)
+{
+    Console.WriteLine(items.ToString());
+}
+Console.WriteLine();
+
+Console.WriteLine("Songs released on October");
+var songsOnOctober = songs.Where(x => x.ReleaseDate.Month == 10).ToList();
+
+foreach (var items in songsOnOctober)
+{
+    Console.WriteLine(items.ToString());
+}
+Console.WriteLine();
+
+Console.WriteLine("Return songs in alphabetical order");
+var songsOrder = songs.OrderBy(x => x.Name).ToList();
+
+foreach (var items in songsOrder)
+{
+    Console.WriteLine(items.ToString());
+}
+Console.WriteLine();
+
+Console.WriteLine("Last two songs registered in alphabetical order");
+var lastTwoSongs = songs.OrderByDescending(x => x.ReleaseDate.Date)
+    .Take(2).OrderBy(x => x.Name).ToList();
+
+foreach (var items in lastTwoSongs)
+{
+    Console.WriteLine(items.ToString());
+}
---
+++ 2DiasDeCodigo/2DiasDeCodigo/Songs.cs
@@ -0,0 +1,25 @@
+﻿
+namespace _2DiasDeCodigo
+{
+    public class Songs
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string Band { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public Songs(int id, string? name, string band, DateTime releaseDate)
+        {
+            Id = id;
+            Name = name;
+            Band = band;
+            ReleaseDate = releaseDate;
+        }
+
+        public override string ToString()
+        {
+            return $"Code: {Id} | Name: {Name} | Band: {Band} " +
+                $"| Release date: {ReleaseDate}";
+        }
+    }
+}
