8706520970be1225389f7de250552b6f800c2563
Willian Gonçalves <santoswil81@gmail.com>
2025-03-01 15:04:31 -0300

    Atualizando lista com novo conteúdo em C#

---
+++ #100DiasDeCodigo/5DiasDeCodigo/5DiasDeCodigo/Program.cs
@@ -0,0 +1,85 @@
+﻿// Implicit Operators
+
+// 1.
+int x = 100;
+double y = x;
+// foi possível converter x de int para double naturalmente,
+// afinal os números reais suportam inteiros.
+
+// 2. 
+double z = 100.38;
+int w = (int)z; // w = 100
+// Os números inteiros não são capazes de resolver números reais.
+// Neste caso, precisamos explicitar a conversão.
+
+//// 3. Sobreescrevendo o método ToString:
+var phone = new Phone
+{
+CountryCode = "55",
+Area = "11",
+Number = "999999999"
+};
+Console.WriteLine(phone);
+
+public class Phone
+{
+    public string? CountryCode { get; set; }
+    public string? Area { get; set; }
+    public string? Number { get; set; }
+
+    public override string ToString()
+    {
+        return $"+{CountryCode} ({Area}) {Number}";
+    }
+}
+
+// 4. Implicit operator (tipo complexo para string):
+var phone = new Phone
+{
+CountryCode = "55",
+Area = "11",
+Number = "999999999"
+};
+var telefone = "123";
+telefone = phone;
+Console.WriteLine(telefone);
+
+public class Phone
+{
+    public string? CountryCode { get; set; }
+    public string? Area { get; set; }
+    public string? Number { get; set; }
+
+    public static implicit operator string(Phone phone)
+    {
+        return $"+{phone.CountryCode} ({phone.Area}) {phone.Number}";
+    }
+}
+
+// 5. Implicit operator (string para tipo complexo):
+var phone = new Phone();
+phone = "55 11 999999999";
+Console.WriteLine(phone);
+
+public class Phone
+{
+    public string? CountryCode { get; set; }
+    public string? Area { get; set; }
+    public string? Number { get; set; }
+
+    public static implicit operator string(Phone phone)
+    {
+        return $"+{phone.CountryCode} ({phone.Area}) {phone.Number}";
+    }
+
+    public static implicit operator Phone(string phone)
+    {
+        var data = phone.Split(' ');
+        return new Phone 
+        { 
+            CountryCode = data[0],
+            Area = data[1],
+            Number = data[2]
+        };
+    }
+}
