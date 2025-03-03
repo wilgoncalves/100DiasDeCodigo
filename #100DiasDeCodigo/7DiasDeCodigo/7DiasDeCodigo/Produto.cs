46768b3aae4b3993aabd86b359a518c97328640e
Willian Gonçalves <santoswil81@gmail.com>
2025-03-03 13:22:10 -0300

    Construtores e sobrecarga

---
+++ #100DiasDeCodigo/7DiasDeCodigo/7DiasDeCodigo/Produto.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+
+namespace _7DiasDeCodigo
+{
+    internal class Produto
+    {
+        public string? Nome;
+        public double Preco;
+        public int Quantidade;
+
+        // Princípio de sobrecarga:
+        public Produto()
+        {
+
+        }
+
+        // Com o intuito de evitar a existência de produtos sem nome e sem preço, é possível
+        // fazer com que seja “obrigatória” a iniciação desses valores por meio dos construtores.
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        // Princípio de sobrecarga:
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 5; // é possível iniciar o atributo com qualquer valor.
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco* Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ #100DiasDeCodigo/7DiasDeCodigo/7DiasDeCodigo/Program.cs
@@ -0,0 +1,45 @@
+﻿using _7DiasDeCodigo;
+using System.Globalization;
+
+namespace _7DiasDeCodigo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine()!;
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+            
+            // Com o princípio de sobrecarga, é possível omitir a entrada de valor de um dos atributos.
+            //Console.Write("Quantidade no estoque: ");
+            //int quantidade = int.Parse(Console.ReadLine()!);
+            // Produto p = new(nome, preco, quantidade);
+
+            // Princípio de sobrecarga:
+            Produto p = new(nome, preco);
+            Produto p2 = new Produto();
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine()!);
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine()!);
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
