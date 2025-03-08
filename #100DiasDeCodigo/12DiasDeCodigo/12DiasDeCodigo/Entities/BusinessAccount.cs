6ca15ad871d046fe3ebd36742e941326c0de812b
Willian Gonçalves <santoswil81@gmail.com>
2025-03-08 14:40:03 -0300

    Herança em C#

---
+++ #100DiasDeCodigo/12DiasDeCodigo/12DiasDeCodigo/Entities/BusinessAccount.cs
@@ -0,0 +1,28 @@
+﻿// Herança
+
+namespace _12DiasDeCodigo.Entities
+{   // A subclasse herda dados e comportamentos da superclasse. - Classe mais específica.
+    internal class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+
+        public BusinessAccount(int number, string holder, 
+            double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ #100DiasDeCodigo/12DiasDeCodigo/12DiasDeCodigo/Program.cs
@@ -0,0 +1,6 @@
+﻿// Herança
+using _12DiasDeCodigo.Entities;
+
+BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+Console.WriteLine(account.Balance);
