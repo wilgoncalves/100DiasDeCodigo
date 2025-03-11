dbffe24d4a30104af0e08fa840c1574afc726c3b
Willian Gonçalves <santoswil81@gmail.com>
2025-03-11 10:10:20 -0300

    Polimorfismo

---
+++ #100DiasDeCodigo/15DiasDeCodigo/15DiasDeCodigo/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,25 @@
+﻿// Polimorfismo
+
+namespace _15DiasDeCodigo.Entities
+{
+    internal class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCHarge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCHarge)
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCHarge = additionalCHarge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCHarge;
+        }
+    }
+}
---
+++ #100DiasDeCodigo/15DiasDeCodigo/15DiasDeCodigo/Program.cs
@@ -0,0 +1,48 @@
+﻿// Polimorfismo: é o recurso que permite que variáveis de um mesmo tipo mais genérico
+// possam apontar para objetos de tipos específicos diferentes, tendo assim comportamentos
+// diferentes conforme cada tipo específico.
+
+using _15DiasDeCodigo.Entities;
+using System.Globalization;
+
+Console.Write("Enter the number of employees: ");
+int employees = int.Parse(Console.ReadLine()!);
+
+List<Employee> list = new List<Employee>();
+
+for (int i = 1; i <= employees; i++)
+{
+    Console.WriteLine($"Employee #{i} data:");
+
+    Console.Write("Outsourced (y/n)? ");
+    char outsourced = char.Parse(Console.ReadLine()!);
+
+    Console.Write("Name: ");
+    string name = Console.ReadLine()!;
+
+    Console.Write("Hours: ");
+    int hours = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Value per hour: ");
+    double valuePerHour = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+    if (outsourced == 'y')
+    {
+        Console.Write("Additional charge: ");
+        double addCharge = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        list.Add(new OutsourcedEmployee(name, hours, valuePerHour, addCharge));
+    }
+    else
+    {
+        list.Add(new Employee(name, hours, valuePerHour));
+    }
+}
+
+// Execução polimórfica:
+Console.WriteLine();
+Console.WriteLine("PAYMENTS:");
+foreach (Employee emp in list)
+{
+    Console.WriteLine($"{emp.Name} - $ {emp.Payment().ToString("F2", CultureInfo.InvariantCulture)}");
+}
+
