4404bcf19ac21001ad483b9a135e743fd4b98e56
Willian Gonçalves <santoswil81@gmail.com>
2025-03-12 10:15:31 -0300

    Classes abstratas

---
+++ #100DiasDeCodigo/16DiasDeCodigo/16DiasDeCodigo/Entities/Account.cs
@@ -0,0 +1,36 @@
+﻿// Classes abstratas
+
+namespace _16DiasDeCodigo.Entities
+{
+    // Classes abstratas não podem ser instanciadas.
+    // A superclasse genérica nos permite tratar de forma 
+    // fácil e uniforme todos os tipos de conta, inclusive com polimorfismo.
+    abstract class Account
+    {
+        public int Number { get; private set; }
+        public string? Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public virtual void Withdraw(double amount)
+        {
+            Balance -= amount + 5.0;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ #100DiasDeCodigo/16DiasDeCodigo/16DiasDeCodigo/Program.cs
@@ -0,0 +1,33 @@
+﻿// Classes abstratas
+using System.Globalization;
+using _16DiasDeCodigo.Entities;
+
+// Mesmo que a classe seja abstrata, ainda podemos chamá-la em uma lista.
+List<Account> list = new List<Account>();
+
+// A superclasse abstrata nos permite adicionar diferentes subclasses numa lista.
+list.Add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+list.Add(new BusinessAccount(1002, "Maria", 500.0, 400.0));
+list.Add(new SavingsAccount(1003, "Bob", 500.0, 0.01));
+list.Add(new BusinessAccount(1004, "Anna", 500.0, 500.0));
+
+double sum = 0.0;
+foreach (var account in list)
+{
+    sum += account.Balance;
+}
+
+Console.WriteLine($"Total balance: {sum.ToString("F2", CultureInfo.InvariantCulture)}");
+
+// Chamadas polimórficas
+foreach (var account in list)
+{
+    account.Withdraw(10.0);
+}
+
+// 
+foreach (var account in list)
+{
+    Console.WriteLine($"Updated balance for account " +
+        $"{account.Number}: {account.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+}
