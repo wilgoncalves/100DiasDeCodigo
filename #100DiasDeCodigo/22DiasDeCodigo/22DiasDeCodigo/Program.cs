fb9a04d939d14c12785734abba85e1a3a561f06e
Willian Gonçalves <santoswil81@gmail.com>
2025-03-18 11:27:23 -0300

    Uso de generics

---
+++ #100DiasDeCodigo/22DiasDeCodigo/22DiasDeCodigo/Program.cs
@@ -0,0 +1,19 @@
+﻿using _22DiasDeCodigo;
+// Generics
+// Permitem que classes, interfaces e métodos
+// possam ser parametrizados por tipos.
+
+// Ao definir um tipo genérico na classe, podemos instanciar qualquer tipo de variável.
+PrintService<int> printService = new PrintService<int>();
+
+Console.Write("How many values? ");
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine()!); 
+    printService.AddValue(x);
+}
+
+printService.Print();
+Console.WriteLine("First: " + printService.First());
