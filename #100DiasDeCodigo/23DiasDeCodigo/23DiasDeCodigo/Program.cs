5bc4850db6e337cb1b59c501d2711d2de09e1cd9
Willian Gonçalves <santoswil81@gmail.com>
2025-03-19 09:56:11 -0300

    Restricoes de Generics

---
+++ #100DiasDeCodigo/23DiasDeCodigo/23DiasDeCodigo/Program.cs
@@ -0,0 +1,25 @@
+﻿using _23DiasDeCodigo.Services;
+using _23DiasDeCodigo.Entities;
+using System.Globalization;
+// Restrições de Generics
+
+List<Product> list = new List<Product>();
+
+Console.Write("Enter N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < n; i++)
+{
+    string[] vet = Console.ReadLine()!.Split(',');
+    string name = vet[0];
+    double price = double.Parse(vet[1], CultureInfo.InvariantCulture);
+    
+    list.Add(new Product(name, price));
+}
+
+CalculationService calculationService = new CalculationService();
+
+Product max = calculationService.Max(list);
+
+Console.WriteLine("Max:");
+Console.WriteLine(max);
---
+++ #100DiasDeCodigo/23DiasDeCodigo/23DiasDeCodigo/Services/CalculationService.cs
@@ -0,0 +1,27 @@
+﻿// Restrições de Generics
+
+namespace _23DiasDeCodigo.Services
+{
+    internal class CalculationService
+    {
+        // Restrigindo o tipo T com IComparable
+        public T Max<T>(List<T> list) where T : IComparable
+        {
+            if (list.Count == 0)
+            {
+                throw new ArgumentException("List can not be empty");
+            }
+
+            T max = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                // Fazendo comparação da forma genérica.
+                if (list[i].CompareTo(max) > 0)
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+    }
+}
