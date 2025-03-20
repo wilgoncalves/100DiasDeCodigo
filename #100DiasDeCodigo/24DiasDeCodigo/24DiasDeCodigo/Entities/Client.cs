b1dc9ab3db5df8ebbb19a0781fc5aa01e9bb59e0
Willian Gonçalves <santoswil81@gmail.com>
2025-03-20 11:40:33 -0300

    GetHashCode e Equals

---
+++ #100DiasDeCodigo/24DiasDeCodigo/24DiasDeCodigo/Entities/Client.cs
@@ -0,0 +1,25 @@
+﻿// GetHashCode e Equals
+
+namespace _24DiasDeCodigo.Entities
+{
+    internal class Client
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+        // Comparando um cliente com outro.
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Client))
+                return false;
+
+            Client? other = obj as Client;
+            return Email!.Equals(other!.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email!.GetHashCode();
+        }
+    }
+}
---
+++ #100DiasDeCodigo/24DiasDeCodigo/24DiasDeCodigo/Program.cs
@@ -0,0 +1,35 @@
+﻿using _24DiasDeCodigo.Entities;
+
+// GetHashCode e Equals
+// São operações da classe Object utilizadas para
+// comparar se um objeto é igual a outro.
+
+// Equals: lento, resposta 100%
+
+string a = "Maria";
+string b = "Alex";
+string c = "Maria";
+
+Console.WriteLine(a.Equals(b));
+Console.WriteLine(a.Equals(c));
+
+// GetHashCode: retorna um número inteiro representando um código.
+
+string d = "Maria";
+string e = "Alex";
+string f = "Maria";
+
+Console.WriteLine(d.GetHashCode());
+Console.WriteLine(e.GetHashCode());
+Console.WriteLine(f.GetHashCode());
+
+
+// Comparando clientes
+
+Client g = new Client { Name = "Maria", Email = "maria@gmail.com" };
+Client h = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+Console.WriteLine(g.Equals(h));
+Console.WriteLine(g == h);
+Console.WriteLine(g.GetHashCode());
+Console.WriteLine(h.GetHashCode());
