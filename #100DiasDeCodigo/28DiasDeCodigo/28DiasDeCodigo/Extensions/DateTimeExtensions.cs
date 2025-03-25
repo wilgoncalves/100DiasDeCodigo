677a3ffe5373c0dbe2168afa50b03fd74343489d
Willian Gonçalves <santoswil81@gmail.com>
2025-03-24 21:12:34 -0300

    Extension methods

---
+++ #100DiasDeCodigo/28DiasDeCodigo/28DiasDeCodigo/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+// Extension Methods
+
+namespace System
+{
+    // A classe precisa ser estática.
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if (duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+            }
+        }
+    }
+}
---
+++ #100DiasDeCodigo/28DiasDeCodigo/28DiasDeCodigo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// Extension Methods
+// São métodos que estendem a funcionalidade de um tipo,
+// sem precisar alterar o código fonte deste tipo, nem herdar desse tipo.
+
+// Retornando data em horas ou dias.
+DateTime dt = new DateTime(2025, 3, 24, 8, 10, 45);
+Console.WriteLine(dt.ElapsedTime());
+
+// Cortando strings
+string s1 = "Good morning dear students!";
+Console.WriteLine(s1.Cut(10));
