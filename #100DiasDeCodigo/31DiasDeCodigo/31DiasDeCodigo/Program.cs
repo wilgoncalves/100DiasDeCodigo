e37a2148c78c511b5ca828bdb28f5a21ae95d283
Willian Gonçalves <santoswil81@gmail.com>
2025-03-27 21:36:12 -0300

    DateTimeKind e ISO 8601

---
+++ #100DiasDeCodigo/31DiasDeCodigo/31DiasDeCodigo/Program.cs
@@ -0,0 +1,44 @@
+﻿// DateTimeKind e padrão ISO 8601
+
+DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local); // Fuso horário do sistema.
+
+DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc); // Fuso horário GMT (Greenwich Mean Time).
+
+DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+
+Console.WriteLine($"d1: {d1}");
+Console.WriteLine($"d1 Kind: {d1.Kind}");
+Console.WriteLine($"d1 to Local: {d1.ToLocalTime()}");
+Console.WriteLine($"d1 to Utc: {d1.ToUniversalTime()}");
+Console.WriteLine();
+
+Console.WriteLine($"d2: {d2}");
+Console.WriteLine($"d2 Kind: {d2.Kind}");
+Console.WriteLine($"d2 to Local: {d2.ToLocalTime()}");
+Console.WriteLine($"d2 to Utc: {d2.ToUniversalTime()}");
+Console.WriteLine();
+
+Console.WriteLine($"d3: {d3}");
+Console.WriteLine($"d3 Kind: {d3.Kind}");
+Console.WriteLine($"d3 to Local: {d3.ToLocalTime()}");
+Console.WriteLine($"d3 to Utc: {d3.ToUniversalTime()}");
+
+// ISO 8601
+
+DateTime d4 = DateTime.Parse("2000-08-15 13:05:58");
+DateTime d5 = DateTime.Parse("2000-08-15T13:05:58Z"); // Padrão ISO 8601
+
+Console.WriteLine($"d4: {d4}");
+Console.WriteLine($"d4 Kind: {d4.Kind}");
+Console.WriteLine($"d4 to Local: {d4.ToLocalTime()}");
+Console.WriteLine($"d4 to Utc: {d4.ToUniversalTime()}");
+Console.WriteLine();
+
+Console.WriteLine($"d5: {d5}");
+Console.WriteLine($"d5 Kind: {d5.Kind}");
+Console.WriteLine($"d5 to Local: {d5.ToLocalTime()}");
+Console.WriteLine($"d5 to Utc: {d5.ToUniversalTime()}");
+Console.WriteLine();
+
+Console.WriteLine(d5.ToString("yyyy-MM-ddTHH:mm:ssZ")); // Importante garantir que o horário esteja no formato universal.
+Console.WriteLine(d5.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")); // Forma mais adequada!
