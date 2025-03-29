91a2cea6e512c1f223a27786b32631ec2e1d65b7
Willian Gonçalves <santoswil81@gmail.com>
2025-03-28 22:08:12 -0300

    Operadores IS e AS

---
+++ #100DiasDeCodigo/32DiasDeCodigo/32DiasDeCodigo/Program.cs
@@ -0,0 +1,49 @@
+﻿// Operador IS
+
+var someText = "This is a string";
+
+Console.WriteLine(someText.GetType());
+
+// Comparando tipos
+var result = someText.GetType() == typeof(string);
+// Melhor forma:
+var result1 = someText is string;
+
+
+// Outras comparações
+var cpf = new CPF();
+if (cpf is Documento)
+Console.WriteLine("CPF é um documento");
+
+public record Documento { }
+public record CPF : Documento { }
+public record CNPJ : Documento { }
+
+
+// Mais comparações
+var student = new Student();
+if (student == null)
+Console.WriteLine("Nulo");
+
+if (student != null)
+Console.WriteLine("Não nulo");
+
+// Aprimorando:
+if (student is null)
+Console.WriteLine("Nulo");
+
+if (student is not null)
+Console.WriteLine("Não nulo");
+
+public record Student { }
+
+
+// Operador AS
+
+var documento = new Documento();
+var cpf = (CPF)documento; // conversão explícita
+var cpf2 = documento as CPF; // Aprimorando com AS
+
+public record Documento { }
+public record CPF : Documento { }
+public record CNPJ : Documento { }
