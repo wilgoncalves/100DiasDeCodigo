106009984b1d1e89770b9ef271620338f8421de0
Willian Gonçalves <santoswil81@gmail.com>
2025-05-02 16:37:24 -0300

    #62diasdecodigo: Sparky Project - Primeiro teste unitario

---
+++ #100DiasDeCodigo/Sparky/Sparky/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Sparky
+{
+    public class Calculator
+    {
+        public int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ #100DiasDeCodigo/Sparky/SparkyMSTest/CalculatorMSTests.cs
@@ -0,0 +1,21 @@
+﻿using Sparky;
+
+namespace SparkyMSTest
+{
+    [TestClass]
+    public class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arange
+            Calculator calc = new();
+
+            //Act
+            int result = calc.AddNumbers(10, 20);
+
+            //Assert
+            Assert.AreEqual(30, result);
+        }
+    }
+}
