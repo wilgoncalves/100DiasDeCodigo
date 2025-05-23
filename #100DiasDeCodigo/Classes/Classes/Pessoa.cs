1bf7420d8c31b61baac03d4d20cbb9388e4f92b5
Willian Gonçalves <santoswil81@gmail.com>
2025-05-22 21:06:42 -0300

    principios solid - classes e objetos

---
+++ #100DiasDeCodigo/Classes/Classes/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿namespace Classes;
+
+public class Pessoa
+{
+    public string? Nome { get; set; }
+    public int Idade { get; set; }
+    public string? Genero { get; set; }
+
+    public Pessoa()
+    {
+    }
+
+    public Pessoa(string? nome, int idade, string? genero)
+    {
+        Nome = nome;
+        Idade = idade;
+        Genero = genero;
+    }
+
+    public Pessoa(string nome)
+    {
+        Nome = nome;
+    }
+
+    public void Identificar()
+    {
+        Console.WriteLine($"Olá, me chamo {Nome}, " +
+            $"tenho {Idade} anos e sou do gênero {Genero}.");
+    }
+}
---
+++ #100DiasDeCodigo/Classes/Classes/Program.cs
@@ -0,0 +1,17 @@
+﻿using Classes;
+
+// construtor sem parâmetros
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "Willian";
+pessoa1.Idade = 26;
+pessoa1.Genero = "masculino";
+pessoa1.Identificar();
+
+Pessoa pessoa2 = new Pessoa("Amanda", 40, "feminino");
+pessoa2.Identificar();
+
+//construtor com apenas um parâmetro
+Pessoa pessoa3 = new Pessoa("Bianca");
+pessoa3.Idade = 18;
+pessoa3.Genero = "feminino";
+pessoa3.Identificar();
