eff8d183d9fe9a11417f541c659e0878e8bbad3c
Willian Gonçalves <santoswil81@gmail.com>
2025-05-25 11:26:43 -0300

    #84diasdecodigo: principios solid - diagrama uml

---
+++ #100DiasDeCodigo/UML-DiagramaClasses/UML-DiagramaClasses/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿namespace UML_DiagramaClasses;
+
+public class Pessoa
+{
+    public Pessoa()
+    {
+    }
+
+    public Pessoa(string nome, int idade, string sexo)
+    {
+        Nome = nome;
+        Idade = idade;
+        Sexo = sexo;
+    }
+
+    private DateTime dataNascimento = new DateTime(1980, 9, 11);
+
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Sexo { get; set; }
+
+    public void IdentificarPessoa()
+    {
+        Console.WriteLine($"{Nome} - {Idade} - {Sexo}");
+    }
+}
